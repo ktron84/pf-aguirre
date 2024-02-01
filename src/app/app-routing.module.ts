6c20d15d01c0b7b3221dbaf7749e31bfd3df9e06
CARLOS EDUARDO AGUIRRE PULGARIN <carlos.aguirre@emtelco.com.co>
2024-02-01 12:25:16 -0500

    Se crea home y menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { LoginComponent } from './layouts/auth/pages/login/login.component';
+import { NotFoundComponent } from './layouts/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard', 
+    component: DashboardComponent,
+    loadChildren: () => import('./layouts/dashboard/dashboard.module').then(
+      m => m.DashboardModule),
+  },
+  {
+    path: 'auth/login',
+    component:LoginComponent,
+  },
+  {
+    path: '404',
+    component: NotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'auth/login'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
