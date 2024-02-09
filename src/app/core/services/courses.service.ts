53df3050f2585c96a07bda604f84bc6dc1f5022c
CARLOS EDUARDO AGUIRRE PULGARIN <carlos.aguirre@emtelco.com.co>
2024-02-09 12:31:12 -0500

    Refactor courses service and courses component

--- src/app/core/services/courses.service.ts
+++ src/app/core/services/courses.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from "@angular/core";
-import { delay, of } from "rxjs";
-import { Course } from "../../layouts/dashboard/pages/courses/models";
+import { Injectable } from '@angular/core';
+import { delay, finalize, of } from 'rxjs';
+import { Course } from '../../layouts/dashboard/pages/courses/models';
+import { LoadingService } from './loading.service';
 
 let courses: Course[] = [
   {
@@ -8,47 +9,48 @@ let courses: Course[] = [
     courseName: 'Angular',
     description: 'Curso de Angular 17',
     startDate: new Date(),
-    endDate: new Date(), 
+    endDate: new Date(),
   },
   {
     id: 2,
     courseName: 'React',
     description: 'Curso de React 18',
     startDate: new Date(),
-    endDate: new Date()
+    endDate: new Date(),
   },
   {
     id: 3,
     courseName: 'Vue',
     description: 'Curso de Vue 3',
     startDate: new Date(),
-    endDate: new Date()
+    endDate: new Date(),
   },
 ];
 
-
 @Injectable()
-
 export class CoursesService {
+  constructor(private loadingService: LoadingService) {}
 
-   getCourses() {
-     return of (courses).pipe(delay(400));
-   }
+  getCourses() {
+    this.loadingService.setIsLoading(true);
+    return of(courses).pipe(
+      delay(400),
+      finalize(() => this.loadingService.setIsLoading(false))
+    );
+  }
 
-   createCourse(data: Course){
-      courses = [...courses, { ...data, id: courses.length + 1, }];
-      return this.getCourses();
-
-   }
-
-
-   deleteCourseById(id: number){
-      courses = courses.filter((el)=> el.id !== id);
-      return this.getCourses();
-   }
-
-   updateCourseById(id: number, data: Course) {
-    courses = courses.map((el) => el.id === id ? { ...el, ...data } : el);
+  createCourse(data: Course) {
+    courses = [...courses, { ...data, id: courses.length + 1 }];
     return this.getCourses();
-  }   
+  }
+
+  deleteCourseById(id: number) {
+    courses = courses.filter((el) => el.id !== id);
+    return this.getCourses();
+  }
+
+  updateCourseById(id: number, data: Course) {
+    courses = courses.map((el) => (el.id === id ? { ...el, ...data } : el));
+    return this.getCourses();
+  }
 }
--- src/app/layouts/dashboard/pages/courses/courses.component.ts
+++ src/app/layouts/dashboard/pages/courses/courses.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output,OnChanges, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnChanges,
+  OnInit,
+} from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../../../../core/services/courses.service';
 import { LoadingService } from '../../../../core/services/loading.service';
@@ -9,20 +16,26 @@ import { CourseDialogComponent } from './components/course-dialog/course-dialog.
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
-
 export class CoursesComponent {
-  displayedColumns: string[] = ['id', 'courseName', 'description', 'startDate', 'endDate', 'actions'];
+  displayedColumns: string[] = [
+    'id',
+    'courseName',
+    'description',
+    'startDate',
+    'endDate',
+    'actions',
+  ];
 
   courses: Course[] = [];
 
-  constructor(private _snackBar:MatSnackBar, 
-    private coursesService: CoursesService, 
+  constructor(
+    private _snackBar: MatSnackBar,
+    private coursesService: CoursesService,
     private LoadingService: LoadingService,
     public dialog: MatDialog
   ) {
-    this.LoadingService.setIsLoading(true);
     this.coursesService.getCourses().subscribe({
       next: (courses) => {
         this.courses = courses;
@@ -30,67 +43,62 @@ export class CoursesComponent {
       complete: () => {
         this.LoadingService.setIsLoading(false);
       },
-    });    
+    });
   }
 
-  onCreateCourse(): void{
-    this.dialog.open(CourseDialogComponent).
-    afterClosed().
-    subscribe({
-      next:(result) => {
-        if(result){
-          this.LoadingService.setIsLoading(true);
-          this.coursesService.createCourse(result).subscribe({
-            next: (courses) => {
-              this.courses = courses;
-              this._snackBar.open('Curso creado correctamente', 'cerrar', {
-                duration: 2000,
-              });
-            },
-            complete: () => {
-              this.LoadingService.setIsLoading(false);
-            },
-          });
-        }
-      }
-    }); 
+  onCreateCourse(): void {
+    this.dialog
+      .open(CourseDialogComponent)
+      .afterClosed()
+      .subscribe({
+        next: (result) => {
+          if (result) {
+            this.coursesService.createCourse(result).subscribe({
+              next: (courses) => {
+                this.courses = courses;
+                this._snackBar.open('Curso creado correctamente', 'cerrar', {
+                  duration: 2000,
+                });
+              },
+            });
+          }
+        },
+      });
   }
 
-  onEditCourse(course: Course){
-    this.dialog.open(CourseDialogComponent, {
-      data: course,
-    }).afterClosed().subscribe({
-      next: (result) => {
-        if (result) {
-          this.LoadingService.setIsLoading(true);
-          this.coursesService.updateCourseById(course.id, result).subscribe({
-            next: (courses) => {
-              this.courses = courses;
-              this._snackBar.open('Curso actualizado correctamente', 'cerrar', {
-                duration: 2000,
-              });
-            },
-            complete: () => {
-              this.LoadingService.setIsLoading(false);
-            },
-          });
-        
-        };
-      }  
-    }); 
+  onEditCourse(course: Course) {
+    this.dialog
+      .open(CourseDialogComponent, {
+        data: course,
+      })
+      .afterClosed()
+      .subscribe({
+        next: (result) => {
+          if (result) {
+            this.coursesService.updateCourseById(course.id, result).subscribe({
+              next: (courses) => {
+                this.courses = courses;
+                this._snackBar.open(
+                  'Curso actualizado correctamente',
+                  'cerrar',
+                  {
+                    duration: 2000,
+                  }
+                );
+              },
+            });
+          }
+        },
+      });
   }
 
   onDeleteCourse(id: number) {
-    this.LoadingService.setIsLoading(true);
     this.coursesService.deleteCourseById(id).subscribe({
       next: (courses) => {
         this.courses = courses;
         this._snackBar.open('Curso eliminado correctamente', 'cerrar', {
           duration: 2000,
         });
-      },
-      complete: () => {
-        this.LoadingService.setIsLoading(false);
       },
     });
   }
