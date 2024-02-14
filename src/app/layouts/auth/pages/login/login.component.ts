e9d69549ff155ca4d416077b37df40491b52891c
CARLOS EDUARDO AGUIRRE PULGARIN <carlos.aguirre@emtelco.com.co>
2024-02-13 20:07:26 -0500

    Add AuthModule and Login

--- src/app/layouts/auth/pages/login/login.component.ts
+++ src/app/layouts/auth/pages/login/login.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  revealPassword = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: this.fb.control('', [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(50),
+      ]),
+      password: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+    } else {
+      console.log(this.loginForm.value);
+    }
+  }
 }
---
+++ src/app/shared/validation-errors.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+@Pipe({
+  name: 'validationErrors',
+})
+export class ValidationErrorsPipe implements PipeTransform {
+  transform(errors?: ValidationErrors | null, ...args: unknown[]): unknown {
+    //console.log(errors);
+    if (!!errors) {
+      let messages = [];
+      if (errors['required']) messages.push('Este campo es requerido');
+      if (errors['email']) messages.push('El email no es válido');
+      if (errors['maxlength'])
+        messages.push(
+          `No puede tener más de ${errors['maxlength']?.requiredLength} caracteres`
+        );
+      if (errors['minlength'])
+        messages.push(
+          `Debe tener más de ${errors['minlength']?.requiredLength} caracteres`
+        );
+
+      return messages.join('. ') + '.';
+    }
+
+    return null;
+  }
+}
